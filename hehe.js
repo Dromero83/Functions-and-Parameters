5be9b96ba70f89cba2a739a405e5ea50d2f16e24
Dromero83 <89482575+Dromero83@users.noreply.github.com>
2023-01-31 22:41:14 -0600

    h
    h

---
+++ hehe.js
@@ -0,0 +1,29 @@
+const sandwich = {
+    bread: 'wheat',
+    meat: 'ham',
+    cheese: 'cheddar',
+    veggietable: 'lettuce',
+    sauce: 'mayonnaise',
+
+name : function() {
+    return this.meat + " sandwich with " + this.sauce;
+}
+};
+
+document.getElementById("food").innerHTML = sandwich.name();
+
+function myfunction(para1, para2){
+console.log(para1 + " " + para2);
+}
+myfunction ("help","me");
+
+function message() {
+    alert ("You're amazing!");
+}
+
+function replace() {
+    let text = document.getElementById("helloworld").innerHTML;
+    document.getElementById("helloworld").innerHTML =
+    text.replace("melancholic song by Louie Zong", "jazzy song by Kenjiro Tsuda");
+}
+
